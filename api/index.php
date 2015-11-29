1ee84f4857c3e07279304c0c3ca0f42d4e645e38
Van-Duyet Le <lvduit08@gmail.com>
2015-11-29 23:49:03 +0700

    AI score api

--- api/index.php
+++ api/index.php
@@ -1,6 +1,15 @@
 <?php
 include_once('./simple_html_dom.php');
 
+if (!isset($_REQUEST['sat_1_reading'])) 			$_REQUEST['sat_1_reading'] = '';
+if (!isset($_REQUEST['sat_1_math'])) 				$_REQUEST['sat_1_math'] = '';
+if (!isset($_REQUEST['sat_subject_first'])) 		$_REQUEST['sat_subject_first'] = '';
+if (!isset($_REQUEST['sat_subject_second'])) 		$_REQUEST['sat_subject_second'] = '';
+if (!isset($_REQUEST['sat_subject_third'])) 		$_REQUEST['sat_subject_third'] = '';
+if (!isset($_REQUEST['class_size'])) 				$_REQUEST['class_size'] = '';
+if (!isset($_REQUEST['rank_type'])) 				$_REQUEST['rank_type'] = '';
+if (!isset($_REQUEST['rank_value'])) 				$_REQUEST['rank_value'] = '';
+
 $data =  array(
     'sat_1_reading' =>  $_REQUEST['sat_1_reading'] or '',
     'sat_1_math' => $_REQUEST['sat_1_math'] or '',
@@ -31,6 +40,7 @@ $fields = array(
 );
 
 //url-ify the data for the POST
+$fields_string = '';
 foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 rtrim($fields_string, '&');
 
@@ -41,6 +51,7 @@ $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL, $url);
 curl_setopt($ch,CURLOPT_POST, count($fields));
 curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
 
 //execute post
 $result = curl_exec($ch);
@@ -55,7 +66,23 @@ if (!$result) die('F*ck!');
 $html = str_get_html($result);
 
 // Find
-$score = 0;
-foreach ($html->find('#section_0 > div > div > h2 > strong > font:nth-child(2)') as $value) {
-	$score = $value->innertext;
+$scores = array();
+foreach ($html->find('#section_0 > div > div > h2 > strong > font') as $e) {
+	$scores[] = $e->innertext;
 }
+
+$result = array(
+	'score' => 0,
+	'rank' => 0
+);
+
+if (isset($score[0]) && isset($score[1])) {
+	$result['score'] = intval($scores[0]);
+	$result['rank'] = intval(trim(str_replace('out of 9', '', $scores[1])));
+}
+
+$result['input'] = $fields;
+$result['time'] = time();
+
+header('Content-Type: application/json');
+echo json_encode($result);
